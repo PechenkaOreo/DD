60bb1eab06697c1c15b4332248d91ffd380a95e1
otbor <otborskyi@gmail.com>
2022-07-04 18:50:27 +0300

    first

---
+++ src/main/java/lesson11.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class lesson11 {
+    public static void main(String[] args) {
+
+
+            List<String> word = new ArrayList<>();
+            word.add("maslo");
+            word.add("kolbasa");
+            word.add("moloko");
+for(String rundom:word){
+    System.out.println(rundom.toUpperCase());
+}
+
+
+        }
+
+        }
+
+
+
+
+
