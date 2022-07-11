b7189520fe714ebba04d2d1d830c83c46283c035
otbor <otborskyi@gmail.com>
2022-07-11 20:36:41 +0300

    first1

--- src/main/java/lesson11.java
+++ src/main/java/lesson11.java
@@ -1,22 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class lesson11 {
     public static void main(String[] args) {
+        String str = "koala","pika", "chu";
+        public static void print (String str){
+
+            Set<String> list = new HashSet<>(Arrays.asList(str.replaceAll(".","").split(", ")));
+            for(String st:list){
+
+            String n = str.substring(0, 1).toUpperCase();
+            String m = str.substring(1);
+            String result = n + m;
 
 
-            List<String> word = new ArrayList<>();
-            word.add("maslo");
-            word.add("kolbasa");
-            word.add("moloko");
-for(String rundom:word){
-    System.out.println(rundom.toUpperCase());
+        }
+
+    }
 }
 
 
-        }
 
-        }
+
+
 
 
 
